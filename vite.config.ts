7b5bce4791db13c8c3f07ad2bc86b3d8bc76ea44
Anton Liada <liadaantom@gmail.com>
2023-03-22 16:12:49 +0200

    deploy file was added

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/image-finder/',
+  base: 'https://anton-liada.github.io/image-finder/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
